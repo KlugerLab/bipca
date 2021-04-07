0ca996ef37a70db439ff4f2b51b4639fee21c78d
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-04-07 14:40:58 -0500

    TABS AND SPACES

--- python/bipca/utils.py
+++ python/bipca/utils.py
@@ -1,59 +1,59 @@
 import numpy as np
 import inspect
 def _is_vector(x):
-	return (x.ndim == 1 or x.shape[0] == 1 or x.shape[1] == 1)
+    return (x.ndim == 1 or x.shape[0] == 1 or x.shape[1] == 1)
 
 def _xor(lst, obj):
-	condeval = [ele==obj for ele in lst]
-	condeval = sum(condeval)
-	return condeval==1
+    condeval = [ele==obj for ele in lst]
+    condeval = sum(condeval)
+    return condeval==1
 
 def _zero_pad_vec(nparray, final_length):
-	# pad a vector (nparray) to have length final_length by adding zeros
-	# adds to the largest axis of the vector if it has 2 dimensions
-	# requires the input to have 1 dimension or at least one dimension has length 1.
-	if (nparray.shape[0]) == final_length:
-		z = nparray
-	else:
-		axis = np.argmax(nparray.shape)
-		pad = final_length - nparray.shape[axis]
-		if nparray.ndim>1:
-			if not 1 in nparray.shape:
-				raise ValueError('Input nparray is not a vector')
-		padshape = list(nparray.shape)
-		padshape[axis] = pad
-		z = np.concatenate((nparray,np.zeros(padshape)),axis=axis)
-	return z
+    # pad a vector (nparray) to have length final_length by adding zeros
+    # adds to the largest axis of the vector if it has 2 dimensions
+    # requires the input to have 1 dimension or at least one dimension has length 1.
+    if (nparray.shape[0]) == final_length:
+        z = nparray
+    else:
+        axis = np.argmax(nparray.shape)
+        pad = final_length - nparray.shape[axis]
+        if nparray.ndim>1:
+            if not 1 in nparray.shape:
+                raise ValueError('Input nparray is not a vector')
+        padshape = list(nparray.shape)
+        padshape[axis] = pad
+        z = np.concatenate((nparray,np.zeros(padshape)),axis=axis)
+    return z
 
 def filter_dict(dict_to_filter, thing_with_kwargs):
-	"""
-	Modified from 
-	https://stackoverflow.com/a/44052550    
-	User "Adviendha"
+    """
+    Modified from 
+    https://stackoverflow.com/a/44052550    
+    User "Adviendha"
 
-	"""
-	sig = inspect.signature(thing_with_kwargs)
-	filter_keys = [param.name for param in sig.parameters.values() if param.kind == param.POSITIONAL_OR_KEYWORD and param.name in dict_to_filter.keys()]
-	filtered_dict = {filter_key:dict_to_filter[filter_key] for filter_key in filter_keys}
-	return filtered_dict
+    """
+    sig = inspect.signature(thing_with_kwargs)
+    filter_keys = [param.name for param in sig.parameters.values() if param.kind == param.POSITIONAL_OR_KEYWORD and param.name in dict_to_filter.keys()]
+    filtered_dict = {filter_key:dict_to_filter[filter_key] for filter_key in filter_keys}
+    return filtered_dict
 
 def ischanged_dict(old_dict, new_dict, keys_ignore = []):
-	ischanged = False
+    ischanged = False
 
-	#check for adding or updating arguments
-	for k in new_dict:
-		ischanged = k not in old_dict or old_dict[k] != new_dict[k]#catch values that are new
-		if ischanged:
-			break
-	#now check for removing arguments
-	if not ischanged:
-		for k in old_dict:
-			if k not in keys_ignore and k not in new_dict:
-				ischanged = True
-				break
-	return ischanged
+    #check for adding or updating arguments
+    for k in new_dict:
+        ischanged = k not in old_dict or old_dict[k] != new_dict[k]#catch values that are new
+        if ischanged:
+            break
+    #now check for removing arguments
+    if not ischanged:
+        for k in old_dict:
+            if k not in keys_ignore and k not in new_dict:
+                ischanged = True
+                break
+    return ischanged
 
-	# pre-processing function that removes 0 rows and columns with the option of adding a small eps to matrix
+    # pre-processing function that removes 0 rows and columns with the option of adding a small eps to matrix
 # to allow for sinkhorn to converge faster/better
 def stabilize_matrix(mat, read_cts = None, add_eps = False, return_zero_indices = False):
     
@@ -64,13 +64,13 @@ def stabilize_matrix(mat, read_cts = None, add_eps = False, return_zero_indices 
     
     tol = 1E-6
     if sparse.issparse(mat):
-    	mat = mat[mat.getnnz(1)>0][:,mat.getnnz(0)>0]
-	else:
-	    zero_rows = np.all(np.abs(mat)<=tol, axis = 1)
-	    zero_cols = np.all(np.abs(mat)<=tol, axis = 0)
-    
-	    mat = mat[~zero_rows,:]
-	    mat = mat[:,~zero_cols]
+        mat = mat[mat.getnnz(1)>0][:,mat.getnnz(0)>0]
+    else:
+        zero_rows = np.all(np.abs(mat)<=tol, axis = 1)
+        zero_cols = np.all(np.abs(mat)<=tol, axis = 0)
+
+        mat = mat[~zero_rows,:]
+        mat = mat[:,~zero_cols]
     
     print("New dimension: ", np.shape(mat))
     
