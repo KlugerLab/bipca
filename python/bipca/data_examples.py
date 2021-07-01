0c88e3b0c7845c8dd313fc873b8a92dbca26e8ba
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-07-01 14:24:46 -0500

    scanpypipeline

--- python/bipca/data_examples.py
+++ python/bipca/data_examples.py
@@ -2,6 +2,8 @@
 """
 import numpy as np
 import numpy.matlib as matlib
+import scanpy as sc
+from anndata import AnnData
 from numpy.random import default_rng
 
 
@@ -45,7 +47,26 @@ def multinomial_data(nrows=500, ncols=1000, rank=10, sample_rate=100, simple=Fal
 	return X, PX
 
 def poisson_data(nrows=500, ncols=1000, rank=10, noise = 1, seed = 42):
-
+	"""Summary
+	
+	Parameters
+	----------
+	nrows : int, optional
+	    Description
+	ncols : int, optional
+	    Description
+	rank : int, optional
+	    Description
+	noise : int, optional
+	    Description
+	seed : int, optional
+	    Description
+	
+	Returns
+	-------
+	TYPE
+	    Description
+	"""
 	rng = default_rng(seed = seed)
 	S = np.exp(2*rng.standard_normal(size=(nrows,rank)));
 	coeff = rng.uniform(size=(rank,ncols));
@@ -54,3 +75,51 @@ def poisson_data(nrows=500, ncols=1000, rank=10, noise = 1, seed = 42):
 	Y = rng.poisson(lam=X);  # Poisson sampling
 
 	return Y, X
+
+
+class ScanpyPipeline(object):
+
+	"""Load an .h5ad raw dataset into ScanPy and run the standard transformations to it.
+	"""
+	
+	def __init__(self, fname, readfun = sc.read_h5ad, adata=None):
+		self.fname = fname
+		if isinstance(adata,AnnData):
+			self.adata_raw = adata
+		else:
+			self.adata_raw = readfun(fname)
+		writename = fname.split('.')
+		writename = writename[:-1]
+		if readfun is not sc.read_h5ad:
+			self.adata_raw.write('.'.join(writename)+'.h5ad')
+
+		self.results_file = '.'.join(writename) + '_standard.h5ad'
+	def fit(self, min_genes = 100, min_cells = 100, 
+		max_n_genes_by_counts = 2500, mt_pct_counts = 5, 
+		target_sum = 1e4,reset=False):
+
+		adata = self.adata_raw.copy()
+		##filter cells and genes
+		sc.pp.filter_cells(adata, min_genes=min_genes)
+		sc.pp.filter_genes(adata, min_cells=min_cells)
+
+		adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
+		sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+
+		adata = adata[adata.obs.n_genes_by_counts < max_n_genes_by_counts, :]
+		adata = adata[adata.obs.pct_counts_mt < mt_pct_counts, :]
+
+		sc.pp.normalize_total(adata, target_sum=target_sum)
+		sc.pp.log1p(adata)
+		sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+
+		adata.raw = adata
+
+		adata = adata[:, adata.var.highly_variable]
+
+		sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
+
+		sc.pp.scale(adata, max_value=10)
+
+		self.adata = adata
+		self.adata.write(self.results_file)
