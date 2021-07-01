5642cf99755a2ec64cfc05a52f91231721a1a666
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-07-01 15:50:22 -0500

    updates to scanpy pipeline

--- python/bipca/data_examples.py
+++ python/bipca/data_examples.py
@@ -96,23 +96,36 @@ class ScanpyPipeline(object):
 		self.results_file = '.'.join(writename) + '_standard.h5ad'
 	def fit(self, min_genes = 100, min_cells = 100, 
 		max_n_genes_by_counts = 2500, mt_pct_counts = 5, 
-		target_sum = 1e4,reset=False):
+		target_sum = 1e4,write=False,reset=False):
 
 		adata = self.adata_raw.copy()
 		##filter cells and genes
-		sc.pp.filter_cells(adata, min_genes=min_genes)
-		sc.pp.filter_genes(adata, min_cells=min_cells)
+		self.cells_kept = sc.pp.filter_cells(adata, min_genes=min_genes,inplace=False)
+		self.genes_kept = sc.pp.filter_genes(adata[self.genes_kept,:], min_cells=min_cells,inplace=False)
 
 		adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
 		sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+		self.cells_kept = self.cells_kept * (adata.obs.n_genes_by_counts < max_n_genes_by_counts)
+		self.cells_kept = self.cells_kept * (adata.obs.pct_counts_mt < mt_pct_counts)
 
-		adata = adata[adata.obs.n_genes_by_counts < max_n_genes_by_counts, :]
-		adata = adata[adata.obs.pct_counts_mt < mt_pct_counts, :]
-
+		for k in adata.obs.keys():
+			if k not in self.adata_raw.obs.keys():
+				try:
+					self.adata_raw.obs[k] = adata.obs[k]
+				except Exception as e:
+					print(e)
+		for k in adata.var.keys():
+			if k not in self.adata_raw.var.keys():
+				try:
+					self.adata_raw.var[k] = adata.var[k]
+				except Exception as e:
+					print(e)
+					
+		adata = adata[self.cells_kept, self.genes_kept]
 		sc.pp.normalize_total(adata, target_sum=target_sum)
 		sc.pp.log1p(adata)
 		sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-
+		self.highly_variable = adata.var.highly_variable
 		adata.raw = adata
 
 		adata = adata[:, adata.var.highly_variable]
@@ -122,4 +135,5 @@ class ScanpyPipeline(object):
 		sc.pp.scale(adata, max_value=10)
 
 		self.adata = adata
-		self.adata.write(self.results_file)
+		if write:
+			self.adata.write(self.results_file)
