6268ad7a3d8369f869d80ae251ad6e44e768d793
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-07-29 16:06:31 -0500

    fix to filter_pbmcs.py

--- experiments/pbmcs/filter_pbmcs.py
+++ experiments/pbmcs/filter_pbmcs.py
@@ -21,7 +21,6 @@ import anndata
 from bipca.data_examples import ScanpyPipeline
 
 
-spipe = ScanpyPipeline(fname = data_h5_file,adata = dataset)
 path_to_data = '../data/'
 data_mtx_file = path_to_data + 'sparse_purified_PBMC_filtered_big.mtx'
 data_h5_file = path_to_data + 'purified_PBMC_filtered_big.h5'
