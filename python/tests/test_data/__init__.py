862c9a8231e1ca0cb0c005eef039c0e85901b878
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-11-08 13:43:14 -0600

    tests

---
+++ python/tests/test_data/__init__.py
@@ -0,0 +1,22 @@
+from os.path import exists
+from os import remove
+import scanpy as sc
+import numpy as np
+import bipca
+
+import pathlib
+test_datadir = str(pathlib.Path(__file__).parent.resolve())
+sc.settings.datasetdir = test_datadir
+path_to_raw_data = str(test_datadir) + '/pbmc3k_raw.h5ad'
+path_to_filtered_data =  str(test_datadir) + '/pbmc3k_filtered.h5ad'
+test_output_path = str(test_datadir) + '/test_pbmc3k_bipca_output.h5ad'
+
+raw_adata = sc.datasets.pbmc3k()
+if exists(path_to_filtered_data):
+	filtered_adata = sc.read_h5ad(path_to_filtered_data)
+else:
+	
+	filtered_adata = raw_adata[:,:1000]
+	sc.pp.filter_cells(filtered_adata, min_genes=10)
+	sc.pp.filter_genes(filtered_adata, min_cells=10)
+	filtered_adata.write(path_to_filtered_data)
