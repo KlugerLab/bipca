3fdc40ac6126589c0c08778d959104e12cd23c23
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-11-16 18:27:36 -0600

    bug in best_subsample_idx

--- python/bipca/bipca.py
+++ python/bipca/bipca.py
@@ -1000,7 +1000,7 @@ class BiPCA(BiPCAEstimator):
                                     }
                             else:
                                 best_submtx_idx = 0
-                                best_subsample_idxs = {'rows':np.arange(X.shape[0])
+                                best_subsample_idxs = {'rows':np.arange(X.shape[0]),
                                                         'columns': np.arange(X.shape[1])}
 
                         #subsample the matrix
