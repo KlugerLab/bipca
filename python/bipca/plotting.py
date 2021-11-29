b78c31be9edd4e1a1c7c3c8ea1ec862495da7096
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-11-29 13:14:09 -0600

    npg colormap

--- python/bipca/plotting.py
+++ python/bipca/plotting.py
@@ -593,3 +593,10 @@ class MajorSymLogLocator(SymmetricalLogLocator):
 
 def symlogfmt(x, pos):
     return f'{x:.6f}'.rstrip('0')
+
+def npg_cmap():
+    string = '"#E64B35FF" "#4DBBD5FF" "#00A087FF" "#3C5488FF" "#F39B7FFF" "#8491B4FF" "#91D1C2FF" "#DC0000FF" "#7E6148FF" "#B09C85FF"'
+    string = string.strip('"')
+    string = string.split(' ')
+    output = [mpl.colors.to_rgb(ele) for ele in string]
+    return mpl.colors.ListedColormap(output), output
