299a13171b3dfde27270d1a58b18554c0cc7b36e
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-10-27 17:33:44 -0400

    improvement to Testing_utils
    TestingError

--- python/tests/testing_utils.py
+++ python/tests/testing_utils.py
@@ -1,17 +1,21 @@
 from functools import wraps
 
+class TestingError(Exception):
+	pass
 def raises(exc, someopt=None):
 	def outer(fn):
 		@wraps(fn)
 		def wrapper(*args, **kwargs):
 			try:
 				fn(*args,**kwargs)
-				return False
+				raise TestingError(f'{fn.__name__} ran without raising {exc.__name__}')
 			except Exception as err:
 				if isinstance(err,exc):
 					return True
-				else:
+				elif isinstance(err, TestingError):
 					raise err
+				else:
+					raise TestingError(f'{fn.__name__} ran without raising {exc.__name__}') from err
 			return False
 		return wrapper
 	return outer
