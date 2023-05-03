dda1c8b3437659ee952275a006a18a0e26db3c05
KevinBastianYang <junchen.k.yang@gmail.com>
2023-05-03 09:41:12 -0400

    update plink parameters

--- python/bipca/experiments/datasets/plink_preprocess.sh
+++ python/bipca/experiments/datasets/plink_preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-plink2 --zst-decompress all_hg38.pgen.zst > all_hg38.pgen
-plink2 --zst-decompress all_hg38.pvar.zst > all_hg38.pvar
-plink2 --pfile all_hg38 --allow-extra-chr --max-alleles 2 --remove deg2_hg38.king.cutoff.out.id --chr 1-22 --maf 0.01 --make-bed -out all_phase3_out
-plink --bfile all_phase3_out --indep 50 5 1.000005
-plink --bfile all_phase3_out --extract plink.prune.in --make-bed -out all_phase3_pruned
+/data/jyc/software/plink2 --zst-decompress all_hg38.pgen.zst > all_hg38.pgen
+/data/jyc/software/plink2 --zst-decompress all_hg38.pvar.zst > all_hg38.pvar
+/data/jyc/software/plink2 --pfile all_hg38 --allow-extra-chr --max-alleles 2 --remove deg2_hg38.king.cutoff.out.id --chr 1-22 --maf 0.1 --make-bed -out all_phase3_out #--maf 0.01
+/data/jyc/software/plink --bfile all_phase3_out --indep 50 5 1.0005 #1.000005
+/data/jyc/software/plink --bfile all_phase3_out --extract plink.prune.in --make-bed -out all_phase3_pruned
