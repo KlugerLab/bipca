run normalization as a function, fix hagemann?

python/bipca/experiments/datasets/datasets.py
@@ -6,6 +6,7 @@ import inspect
 from shutil import move as mv, rmtree, copyfileobj
 from numbers import Number
 from typing import Dict
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -26,6 +27,7 @@ from bipca.experiments.datasets.modalities import *
 import subprocess
 from pandas_plink import read_plink
 import pyreadr
+
 
 def get_all_datasets():
     return [
@@ -796,18 +798,22 @@ class HagemannJensen2022(SmartSeqV3):
     )
 
     _raw_urls = {
-        "UMIs.txt": (
-            "https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/"
-            "PBMCs.allruns.umicounts_intronexon.txt"
-        ),
-        "reads.txt": (
-            "https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/"
-            "PBMCs.allruns.readcounts_intronexon.txt"
-        ),
-        "annotations.txt": (
-            "https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/"
-            "PBMCs.allruns.barcode_annotation.txt"
-        ),
+        "raw.zip": "/banach1/jay/bipca_raw_data/hagemannjensen2022/raw.zip"
+        # obtained from a redirect of
+        # ("https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/zip")
+        # the original files are here:
+        # "UMIs.txt": (
+        #     "https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/"
+        #     "PBMCs.allruns.umicounts_intronexon.txt"
+        # ),
+        # "reads.txt": (
+        #     "https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/"
+        #     "PBMCs.allruns.readcounts_intronexon.txt"
+        # ),
+        # "annotations.txt": (
+        #     "https://www.ebi.ac.uk/biostudies/files/E-MTAB-11452/"
+        #     "PBMCs.allruns.barcode_annotation.txt"
+        # ),
     }
     _unfiltered_urls = {None: None}
     _filters = AnnDataFilters(
@@ -831,18 +837,27 @@ class HagemannJensen2022(SmartSeqV3):
 
     def _process_raw_data(self) -> AnnData:
         # first load the umis, read counts, and the barcode annotations.
-        base_files = [v for k, v in self.raw_files_paths.items()]
+        base_files = {
+            "annotations.txt": "PBMCs.allruns.barcode_annotation.txt",
+            "reads.txt": "PBMCs.allruns.readcounts_intronexon.txt",
+            "UMIs.txt": "PBMCs.allruns.umicounts_intronexon.txt",
+        }
+        base_files = {self.raw_files_directory / k: v for k, v in base_files.items()}
+        with zipfile.ZipFile(self.raw_files_directory / "raw.zip", "r") as zip_ref:
+            for target, member in base_files.items():
+                with zip_ref.open(member) as source, open(target, "wb") as target:
+                    copyfileobj(source, target)
 
         data = {
             fname: df
             for fname, df in map(
                 lambda f: (
-                    f.stem,
+                    Path(f).stem,
                     read_csv_pyarrow_bad_colnames(
                         f, delimiter="\t", logger=self.logger
                     ),
                 ),
-                base_files,
+                base_files.keys(),
             )
         }
 
@@ -855,8 +870,8 @@ class HagemannJensen2022(SmartSeqV3):
         del data["reads"]
 
         for col in data["annotations"].columns:
-            if data["annotations"][col].dtype == "object":
-                data["annotations"][col] = data["annotations"][col].astype("category")
+            if data["annotations"][col].dtype in ["object", "category"]:
+                data["annotations"][col] = data["annotations"][col].astype(str)
 
         data["annotations"] = data["annotations"].rename(
             columns={"nReads": "total_reads", "QC_status": "passed_qc"}
@@ -865,7 +880,9 @@ class HagemannJensen2022(SmartSeqV3):
         adata.obs = pd.concat([adata.obs, data["annotations"]], axis=1)
         gene_dict = get_ensembl_mappings(adata.var_names.tolist(), logger=self.logger)
         var_df = pd.DataFrame.from_dict(gene_dict, orient="index")
-        var_df["gene_biotype"] = var_df["gene_biotype"].astype("category")
+        for c in var_df.columns:
+            if var_df[c].dtype in ["object", "category"]:
+                var_df[c] = var_df[c].astype(str)
         adata.var = var_df
         return adata
 
@@ -1195,6 +1212,10 @@ class Zheng2017(TenXChromiumRNAV1):
 
     _unfiltered_urls = {f'{k.split(".")[0]}.h5ad': None for k in _raw_urls.keys()}
     _unfiltered_urls["full.h5ad"] = None
+    _unfiltered_urls["markers.h5ad"] = None  # this is the marker genes figure sample
+    _unfiltered_urls["classifier.h5ad"] = None  # this is the classifier sample
+    # from the paper.
+    _hidden_samples = ["markers.h5ad", "classifier.h5ad"]
     _filters = AnnDataFilters(
         obs={"total_genes": {"min": 100}, "pct_MT_UMIs": {"max": 0.1}},
         var={"total_cells": {"min": 100}},
@@ -1219,6 +1240,41 @@ class Zheng2017(TenXChromiumRNAV1):
         # rm any extracted data.
         rmtree((self.raw_files_directory / "filtered_matrices_mex").resolve())
         data["full"] = ad.concat([data for label, data in data.items()])
+        data["full"].obs_names_make_unique()
+        data["full"].obs["cluster"] = data["full"].obs["cluster"].astype("category")
+        # get the fig4 4 clusters
+        adata = data["full"]
+        mask = ~adata.obs["cluster"].isin(
+            ["CD14+", "CD34+"]
+        )  # CD14 and C34 cells are not in marker genes figure
+        adata = adata[mask, :].copy()
+        # reannotate for the marker gene clusters
+        adata.obs["cluster"] = adata.obs["cluster"].cat.add_categories(
+            ["CD4+ T", "CD8+ T"]
+        )
+
+        adata.obs["cluster"][
+            adata.obs["cluster"].isin(
+                [
+                    "Naive T",
+                    "Treg",
+                    "Helper T",
+                    "Memory T",
+                ]
+            )
+        ] = "CD4+ T"
+        adata.obs["cluster"][
+            adata.obs["cluster"].isin(["Cytotoxic T", "Naive cytotoxic T"])
+        ] = "CD8+ T"
+        adata.obs["cluster"][adata.obs["cluster"].isin(["B"])] = "B"
+        adata.obs["cluster"][adata.obs["cluster"].isin(["CD56+ NK"])] = "CD56+ NK"
+
+        adata.obs["cluster"] = adata.obs["cluster"].cat.remove_unused_categories()
+        data["markers"] = adata
+
+        data["classifier"] = data["full"][
+            data["full"].obs["cluster"] != "Helper T", :
+        ].copy()
         return data
 
 
@@ -1248,63 +1304,59 @@ class SCORCH_INS_OUD(TenXChromiumRNAV3):
 
         return next(iter(adata.values()))
 
-    
+
 #########################################################
 ###               CITE-seq (RNA)                     ###
 #########################################################
-    
+
 
 class Cbmc_citeseq_rna(CITEseq_rna):
     _citation = (
-       " @article{stoeckius2017simultaneous,\n"
-       " title={Simultaneous epitope and transcriptome measurement in single cells},\n"
-       " author={Stoeckius, Marlon and Hafemeister, Christoph and Stephenson, "
-       " William and Houck-Loomis, Brian and Chattopadhyay, Pratip K and Swerdlow, "
-       " Harold and Satija, Rahul and Smibert, Peter},\n"
-       " journal={Nature methods},"
-       " volume={14},"
-       " number={9},"
-       " pages={865--868},"
-       " year={2017},"
-       " publisher={Nature Publishing Group}"
-       "       }"
+        " @article{stoeckius2017simultaneous,\n"
+        " title={Simultaneous epitope and transcriptome measurement in single cells},\n"
+        " author={Stoeckius, Marlon and Hafemeister, Christoph and Stephenson, "
+        " William and Houck-Loomis, Brian and Chattopadhyay, Pratip K and Swerdlow, "
+        " Harold and Satija, Rahul and Smibert, Peter},\n"
+        " journal={Nature methods},"
+        " volume={14},"
+        " number={9},"
+        " pages={865--868},"
+        " year={2017},"
+        " publisher={Nature Publishing Group}"
+        "       }"
     )
     _raw_urls = {
         "cbmc_rna.csv.gz": (
             "https://ftp.ncbi.nlm.nih.gov/geo/series/GSE100nnn/"
             "GSE100866/suppl/GSE100866_CBMC_8K_13AB_10X-RNA_umi.csv.gz"
         ),
-        "cbmc.SeuratData.tar.gz":(
+        "cbmc.SeuratData.tar.gz": (
             "http://seurat.nygenome.org/src/contrib/cbmc.SeuratData_3.1.4.tar.gz"
-        )
+        ),
     }
-    
-    
+
     _unfiltered_urls = {None: None}
-    
+
     def __init__(self, n_filter_iters=1, *args, **kwargs):
         # change default here so that it doesn’t intersect between samples.
         kwargs["n_filter_iters"] = n_filter_iters
         super().__init__(*args, **kwargs)
-        
-        
-    
+
     def _process_raw_data(self) -> AnnData:
-        
         counts_rna_in = self.raw_files_directory / "cbmc_rna.csv.gz"
         metadata_in_parent = self.raw_files_directory / "cbmc.SeuratData.tar.gz"
-        
+
         counts_rna_output = self.raw_files_directory / "cbmc_rna.csv"
-        
+
         # unzip each file
         # read in the rna data as adata, add the metadata inside
         with gzip.open(counts_rna_in) as counts_rna:
             with open(counts_rna_output, "wb") as f_out:
-                    copyfileobj(counts_rna, f_out)
+                copyfileobj(counts_rna, f_out)
         adata = read_csv_pyarrow_bad_colnames(
             counts_rna_output, delimiter=",", index_col=0
         )
-        # note: data is tranposed     
+        # note: data is tranposed
         var_names = adata.index.values
         obs_names = adata.columns
         adata = AnnData(
@@ -1313,84 +1365,95 @@ class Cbmc_citeseq_rna(CITEseq_rna):
         )
         adata.obs_names = obs_names
         adata.var_names = var_names
-        
-        
+
         with self.logger.log_task(f"extracting {metadata_in_parent.name}"):
-                tarfile.open(str(metadata_in_parent)).extractall(self.raw_files_directory)
-        cbmc_meta = pyreadr.read_r(str(self.raw_files_directory) + '/cbmc.SeuratData/inst/extdata/annotations/annotations.Rds')[None]
-        
-        adata.obs['protein_annotations'] = cbmc_meta.loc[adata.obs_names.values,"protein_annotations"]
-        adata.obs['rna_annotations'] = cbmc_meta.loc[adata.obs_names.values,"rna_annotations"]
-        
+            tarfile.open(str(metadata_in_parent)).extractall(self.raw_files_directory)
+        cbmc_meta = pyreadr.read_r(
+            str(self.raw_files_directory)
+            + "/cbmc.SeuratData/inst/extdata/annotations/annotations.Rds"
+        )[None]
+
+        adata.obs["protein_annotations"] = cbmc_meta.loc[
+            adata.obs_names.values, "protein_annotations"
+        ]
+        adata.obs["rna_annotations"] = cbmc_meta.loc[
+            adata.obs_names.values, "rna_annotations"
+        ]
+
         # remove some cell types
-        adata.obs['passQC'] = (adata.obs['protein_annotations'].notna() & (~adata.obs['rna_annotations'].isin(['Eryth','Mk','DC','T/Mono doublets'])) &
-                                (~adata.obs['protein_annotations'].isin(['T/Mono doublets'])))
+        adata.obs["passQC"] = (
+            adata.obs["protein_annotations"].notna()
+            & (
+                ~adata.obs["rna_annotations"].isin(
+                    ["Eryth", "Mk", "DC", "T/Mono doublets"]
+                )
+            )
+            & (~adata.obs["protein_annotations"].isin(["T/Mono doublets"]))
+        )
 
         adata.var["isMouse"] = adata.var_names.str.contains("MOUSE")
         adata.var["total_UMIs"] = np.asarray(adata.X.sum(0)).squeeze()
-        
+
         # only keep the top 100 most highly expressed mouse genes
-        adata.var['Genes2keep'] = adata.var_names.isin(
-            adata.var.loc[adata.var['isMouse'],'total_UMIs'].sort_values(
-                ascending=False)[:100].index) | (~ adata.var["isMouse"])
-        
-        adata = adata[:,adata.var['Genes2keep']]
-        adata = adata[adata.obs['passQC'],:]
-        
-        
+        adata.var["Genes2keep"] = adata.var_names.isin(
+            adata.var.loc[adata.var["isMouse"], "total_UMIs"]
+            .sort_values(ascending=False)[:100]
+            .index
+        ) | (~adata.var["isMouse"])
+
+        adata = adata[:, adata.var["Genes2keep"]]
+        adata = adata[adata.obs["passQC"], :]
+
         return adata
-    
+
+
 class Bm_citeseq_rna(CITEseq_rna):
     _citation = (
-      " @article{stuart2019comprehensive,\n"
-      " title={Comprehensive integration of single-cell data},\n"
-      " author={Stuart, Tim and Butler, Andrew and Hoffman, "
-      " Paul and Hafemeister, Christoph and Papalexi, Efthymia and Mauck, "
-      " William M and Hao, Yuhan and Stoeckius, Marlon and Smibert, "
-      " Peter and Satija, Rahul},\n"
-      " journal={Cell},"
-      " volume={177},"
-      " number={7},"
-      " pages={1888--1902},"
-      " year={2019},"
-      " publisher={Elsevier}"
-      " }"
+        " @article{stuart2019comprehensive,\n"
+        " title={Comprehensive integration of single-cell data},\n"
+        " author={Stuart, Tim and Butler, Andrew and Hoffman, "
+        " Paul and Hafemeister, Christoph and Papalexi, Efthymia and Mauck, "
+        " William M and Hao, Yuhan and Stoeckius, Marlon and Smibert, "
+        " Peter and Satija, Rahul},\n"
+        " journal={Cell},"
+        " volume={177},"
+        " number={7},"
+        " pages={1888--1902},"
+        " year={2019},"
+        " publisher={Elsevier}"
+        " }"
     )
     _raw_urls = {
         "bmcite_rna.tsv.gz": (
-           "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM3681nnn/"
+            "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM3681nnn/"
             "GSM3681518/suppl/GSM3681518_MNC_RNA_counts.tsv.gz"
         ),
-        "bmcite_metadata.tsv":(
+        "bmcite_metadata.tsv": (
             "https://www.dropbox.com/s/g2jm3m3uupqwdlc/bmcite_metadata.tsv?dl=1"
-        )
+        ),
     }
-    
-    
+
     _unfiltered_urls = {None: None}
-    
+
     def __init__(self, n_filter_iters=1, *args, **kwargs):
         kwargs["n_filter_iters"] = n_filter_iters
         super().__init__(*args, **kwargs)
-        
-        
-    
+
     def _process_raw_data(self) -> AnnData:
-        
         counts_rna_in = self.raw_files_directory / "bmcite_rna.tsv.gz"
         metadata_in = self.raw_files_directory / "bmcite_metadata.tsv"
-        
+
         counts_rna_output = self.raw_files_directory / "bmcite_rna.tsv"
-        
+
         # unzip each file
         # read in the rna data as adata, add the metadata inside
         with gzip.open(counts_rna_in) as counts_rna:
             with open(counts_rna_output, "wb") as f_out:
-                    copyfileobj(counts_rna, f_out)
+                copyfileobj(counts_rna, f_out)
         adata = read_csv_pyarrow_bad_colnames(
             counts_rna_output, delimiter="\t", index_col=0
         )
-        # note: data is transposed     
+        # note: data is transposed
         var_names = adata.index.values
         obs_names = adata.columns
         adata = AnnData(
@@ -1399,21 +1462,20 @@ class Bm_citeseq_rna(CITEseq_rna):
         )
         adata.obs_names = obs_names
         adata.var_names = var_names
-        
-        
+
         bm_meta = read_csv_pyarrow_bad_colnames(
             metadata_in, delimiter="\t", index_col=0
         )
-        
-        bm_meta.index = bm_meta.index.str.replace('-','.')
-        
-        cells2keep = np.intersect1d(bm_meta.index,adata.obs_names)
-        adata = adata[cells2keep,:]
-        bm_meta = bm_meta.loc[cells2keep,:]
-        adata.obs['cell_types'] = bm_meta.loc[adata.obs_names.values,"celltype.l2"]
-        
-         
+
+        bm_meta.index = bm_meta.index.str.replace("-", ".")
+
+        cells2keep = np.intersect1d(bm_meta.index, adata.obs_names)
+        adata = adata[cells2keep, :]
+        bm_meta = bm_meta.loc[cells2keep, :]
+        adata.obs["cell_types"] = bm_meta.loc[adata.obs_names.values, "celltype.l2"]
+
         return adata
+
 
 #############################################################
 ###               1000 Genome Phase3                      ###
@@ -1423,7 +1485,8 @@ class Bm_citeseq_rna(CITEseq_rna):
 class Phase3_1000Genome(SingleNucleotidePolymorphism):
     """
     Dataset class to obtain 1000 genome phase3 SNP data
-    Note: This class is dependent on plink/plink2 (bash) and a python package pandas_plink.
+    Note: This class is dependent on plink/plink2 (bash) and a python package
+    pandas_plink.
 
         plink and plink2 need to be added to the environment variables
 

python/bipca/experiments/datasets/modalities.py
@@ -50,10 +50,7 @@ class Simulation(Modality, Technology):
 
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
+        annotations = AnnDataAnnotations.from_other(adata)
         annotations.obs["total_nz"] = nz_along(adata.X, 1)
         annotations.var["total_nz"] = nz_along(adata.X, 0)
         return annotations
@@ -107,10 +104,7 @@ class SingleCellATACSeq(Modality):
 
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
+        annotations = AnnDataAnnotations.from_other(adata)
         annotations.obs["total_sites"] = nz_along(adata.X, 1)
         annotations.obs["total_peaks"] = np.asarray(adata.X.sum(1)).squeeze()
         annotations.var["total_cells"] = nz_along(adata.X, 0)
@@ -168,18 +162,16 @@ class SingleCellRNASeq(Modality):
 
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
+        annotations = AnnDataAnnotations.from_other(adata)
+
         try:
-            gn = adata.var["gene_name"]
+            gn = annotations.var["gene_name"]
         except Exception:
             gn = adata.var_names
         # gene annotations
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
         annotations.var["total_UMIs"] = np.asarray(adata.X.sum(0)).squeeze()
         annotations.var["total_cells"] = nz_along(adata.X, 0)
-        annotations.var["is_MT"] = gn.str.lower().str.startswith("mt-")
+        annotations.var["is_MT"] = gn.str.lower().str.startswith("mt-").astype(bool)
         # cell annotations
         annotations.obs["total_UMIs"] = np.asarray(adata.X.sum(1)).squeeze()
         annotations.obs["total_MT_UMIs"] = np.asarray(
@@ -220,10 +212,7 @@ class SmartSeqV3(SingleCellRNASeq, Technology):
 
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
+        annotations = AnnDataAnnotations.from_other(adata)
         if (reads := adata.layers.get("reads", None)) is not None:
             assert hasattr(adata.obs, "total_reads")
             # read annotations of genes
@@ -294,10 +283,7 @@ class SingleNucleotidePolymorphism(Modality, Technology):
 
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
+        annotations = AnnDataAnnotations.from_other(adata)
         annotations.obs["total_SNPs"] = nz_along(adata.X, axis=1)
         annotations.obs["total_counts"] = np.asarray(adata.X.sum(1)).squeeze()
         annotations.var["total_obs"] = nz_along(adata.X, axis=0)
@@ -305,37 +291,32 @@ class SingleNucleotidePolymorphism(Modality, Technology):
 
         return annotations
 
+
 ###################################################
 ###                   CITE-seq (RNA            ###
 ###################################################
 
-    
-class CITEseq_rna(Modality):
-    """CITEseq_rna: The RNA modality of the CITE-seq technology.
-    """
 
-    _filters = AnnDataFilters(
-        obs={"total_genes": None}, var={"total_cells": None}
-    )
+class CITEseq_rna(Modality):
+    """CITEseq_rna: The RNA modality of the CITE-seq technology."""
+
+    _filters = AnnDataFilters(obs={"total_genes": None}, var={"total_cells": None})
     _filters = AnnDataFilters(
         obs={"total_genes": {"min": 100}},
         var={"total_cells": {"min": 100}},
     )
-        
+
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
+        annotations = AnnDataAnnotations.from_other(adata)
         annotations.obs["total_genes"] = nz_along(adata.X, 1)
         annotations.obs["total_UMIs"] = np.asarray(adata.X.sum(1)).squeeze()
         annotations.var["total_cells"] = nz_along(adata.X, 0)
         annotations.var["total_UMIs"] = np.asarray(adata.X.sum(0)).squeeze()
-        
-        
+
         return annotations
-    
+
+
 ###################################################
 #   Spatial Transcriptomics and technologies      #
 ###################################################
@@ -349,10 +330,7 @@ class SpatialTranscriptomics(Modality):
 
     @classmethod
     def _annotate(cls, adata: AnnData) -> AnnDataAnnotations:
-        annotations = AnnDataAnnotations(
-            obs=DataFrame(index=adata.obs.index),
-            var=DataFrame(index=adata.var.index),
-        )
+        annotations = AnnDataAnnotations.from_other(adata)
         annotations.obs["total_genes"] = nz_along(adata.X, axis=1)
         annotations.obs["total_counts"] = np.asarray(adata.X.sum(1)).squeeze()
         annotations.var["total_obs"] = nz_along(adata.X, axis=0)

python/bipca/experiments/experiments.py
@@ -24,8 +24,6 @@ from scipy.stats import chi2
 from collections import OrderedDict
 
 
-
-
 def knn_classifier(
     X=None,
     labels_true=None,
@@ -392,6 +390,7 @@ def log1p(A, scale="median"):
     if scale != "median" and not isinstance(scale, Number):
         raise ValueError("`scale` must be 'median' or a Number")
     libsize = safe_dim_sum(X, dim=1)
+
     if scale == "median":
         scale = np.median(libsize)
     scale = scale / libsize

python/bipca/experiments/figures/figures.py
@@ -1,4 +1,6 @@
+import os, errno
 import itertools
+import subprocess
 from pathlib import Path
 from functools import partial, singledispatch
 from typing import Dict, Union, Optional, List
@@ -6,7 +8,12 @@ from typing import Dict, Union, Optional, List
 
 import numpy as np
 import pandas as pd
-from anndata import AnnData
+from anndata import AnnData, read_h5ad
+import scanpy as sc
+from scipy import sparse
+from scipy.io import mmwrite
+from scipy.stats import zscore
+from ALRA import ALRA
 
 import bipca
 from bipca import BiPCA
@@ -29,6 +36,7 @@ from .utils import (
     compute_axis_limits,
 )
 import bipca.experiments.datasets as bipca_datasets
+from bipca.experiments.experiments import log1p
 
 
 def extract_dataset_parameters(
@@ -126,6 +134,8 @@ def run_all(
         to_compute = []
         d = dataset()
         for sample in d.samples:
+            if sample in d.hidden_samples:
+                continue
             if [d.__class__.__name__, sample] in written_datasets_samples:
                 continue
             else:
@@ -139,6 +149,119 @@ def run_all(
             # df.drop(["Dataset", "Sample"], axis="columns")
             df.to_csv(csv_path, mode="a", header=False)
     return pd.read_csv(csv_path)
+
+
+def apply_normalizations(adata_path: Union[Path, str], n_threads=32, no=[]):
+    """
+    adata_path: path to the input anndata file which stores the raw count as adata.X
+    output_path: path to store the output adata that will store the normalized data matrices,
+                 and a tmp folder that store the intermediate files from sanity
+    output_adata: output name for the normalized adata
+    n_threads: number of threads to run Sanity Default: 10
+    no: a array of which methods not to run, including log1p, sctransform, alra, sanity Default: empty array
+    """
+
+    # Read data
+    print("Loading count data ...\n")
+    try:
+        adata = read_h5ad(adata_path)
+    except FileNotFoundError:
+        print("Error: Unable to find the h5ad file")
+
+    # convert to sparse matrix
+    if issparse(adata.X):
+        X = adata.X
+    else:
+        X = sparse.csr_matrix(adata.X)
+
+    # If no, else run log1p
+    if ("log" in no) | ("log1p" in no) | ("logtransform" in no):
+        pass
+    else:
+        print("Running log normalization ...\n")
+        adata.layers["log1p"] = log1p(X)
+        adata.layers["log1p+z"] = sparse.csr_matrix(
+            zscore(adata.layers["log1p"].toarray(), axis=0)
+        )
+    # If no, else run sctransform
+    if "sct" in no:
+        pass
+    else:
+        print("Running analytical pearson residuals ...\n")
+        result_dict = sc.experimental.pp.normalize_pearson_residuals(
+            adata, inplace=False
+        )
+        adata.layers["Pearson"] = result_dict["X"]
+    # If no, else run alra
+    if "alra" in no:
+        pass
+    else:
+        print("Running ALRA ...\n")
+        adata.layers["ALRA"] = ALRA(X)
+
+    # If no, else run sanity
+    if "sanity" in no:
+        pass
+    else:
+        print("Running Sanity ...\n")
+        # Mounted to where sanity is installed
+        sanity_installation_path = "/Sanity/bin/Sanity"
+        # Specify the temporary folder that will store the output from intermediate outputs from Sanity
+        tmp_path_sanity = Path(adata_path).parent / "tmp"
+        tmp_path_sanity.mkdir(parents=True, exist_ok=True)
+        # write intermediate files from sanity
+        sanity_counts_path = tmp_path_sanity / "count.mtx"
+        sanity_cells_path = tmp_path_sanity / "barcodes.tsv"
+        sanity_genes_path = tmp_path_sanity / "genes.tsv"
+        mmwrite(str(sanity_counts_path), X.T)
+        pd.Series(adata.obs_names).to_csv(
+            sanity_cells_path,
+            sep="\t",
+            index=False,
+            header=None,
+        )
+        pd.Series(adata.var_names).to_csv(
+            sanity_genes_path, sep="\t", index=False, header=None
+        )
+
+        # hack the count mtx because sanity can't handle 2nd % from mmwrite
+        # with open((tmp_path_sanity + "/count_tmp.mtx"), "r") as file_input:
+        #    file_orig = file_input.readlines()
+        #    file_rev = file_orig[::-1]
+        #    with open((tmp_path_sanity + "/count.mtx"), "w") as output:
+        #        output.write(file_orig[0])
+        #        output.write(file_orig[2])
+        #        for i,line in enumerate(file_rev[:-3]):
+        #                output.write(line)
+
+        sanity_command = (
+            sanity_installation_path
+            + " -f "
+            + str(sanity_counts_path)
+            + " -mtx_genes "
+            + str(sanity_genes_path)
+            + " -mtx_cells "
+            + str(sanity_cells_path)
+            + " -d "
+            + str(tmp_path_sanity)
+            + " -n "
+            + str(n_threads)
+        )
+        subprocess.run(sanity_command.split())
+        # print(error)
+        sanity_output = tmp_path_sanity / "log_transcription_quotients.txt"
+        adata.layers["Sanity"] = (
+            pd.read_csv(
+                sanity_output,
+                sep="\t",
+                index_col=0,
+            )
+            .to_numpy()
+            .T
+        )
+
+    adata.write(adata_path)
+    return adata
 
 
 class Figure2(Figure):