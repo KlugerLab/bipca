21cddf970de007d214a71637190120b56325867f
Jay Stanley <jay.s.stanley.3@gmail.com>
2024-02-29 14:28:29 -0500

    log func_with

--- python/bipca/experiments/base.py
+++ python/bipca/experiments/base.py
@@ -1,3 +1,3 @@
-from bipca.base import ABC, abstractclassattribute, abstractmethod, classproperty
+from bipca.base import ABC, abstractclassattribute, abstractmethod, classproperty,log_func_with
 
 
