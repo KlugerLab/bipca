add library normalization, fix issues with out-of-date sigma/b/c/variance

python/bipca/bipca.py
@@ -24,6 +24,7 @@ from .math import (
     SamplingMatrix,
     minimize_chebfun,
 )
+from .math import library_normalize as lib_normalize
 from .utils import (
     stabilize_matrix,
     filter_dict_with_kwargs,
@@ -730,6 +731,7 @@ class BiPCA(BiPCAEstimator):
             self._M, self._N = A.shape
 
             X, A = self.process_input_data(A)
+            self.library_size = np.median(np.asarray(sum(X,axis =1)))
             self.reset_submatrices(X=X)
             if self.k == -1:  # k is determined by the minimum dimension
                 self.k = np.min([self.M, self.N])
@@ -841,11 +843,9 @@ class BiPCA(BiPCAEstimator):
                         " fitting with a larger k = {}".format(self.k)
                     )
             if self.variance_estimator == "quadratic" and self.fit_sigma == True:
-                self.sigma = np.sqrt(self.sigma**2 * self.shrinker.sigma**2)
-                self.bhat = self.compute_bhat(self.q, self.sigma)
-                self.chat = self.compute_chat(self.q, self.sigma)
-                self.b
-                self.c
+                self._update_quadratic_parameters(self.shrinker.sigma)
+                
+                
 
             del M
             del X
@@ -859,6 +859,7 @@ class BiPCA(BiPCAEstimator):
         counts=True,
         which="left",
         unscale=False,
+        library_normalize=True,
         shrinker=None,
         denoised=True,
         truncate=0,
@@ -877,6 +878,12 @@ class BiPCA(BiPCAEstimator):
             Which principal components to return. By default, the left (row-wise) PCs are returned.
         unscale : bool, default False
             Unscale the output matrix so that it is in the original input domain.
+        library_normalize : float or bool, default True
+            Perform library normalization on the output matrix. If a float, then the 
+            output matrix is divided by the sum of its column counts and multiplied by 
+            the float. If False, no library normalization is performed.
+            If True, the output matrix is divided by the sum of its column counts and 
+            multiplied by the median of the input matrix column sums.
         shrinker : {'hard','soft', 'frobenius', 'operator','nuclear'}, optional
             Shrinker to use for denoising
             (Defaults to `obj.default_shrinker`)
@@ -922,7 +929,15 @@ class BiPCA(BiPCAEstimator):
                     Z = where(less_equal(Z, thresh), 0, Z)
             if unscale:
                 Z = self.unscale(Z)
-
+            if library_normalize:
+                if library_normalize is True:
+                    if X is not None:
+                        scale = np.median(np.asarray(sum(X, axis=1)))
+                    else:
+                        scale = self.library_size
+                elif isinstance(library_normalize, Number):
+                    scale = library_normalize
+                Z = lib_normalize(Z,scale = scale)
             return Z
 
         else:
@@ -1721,6 +1736,22 @@ class BiPCA(BiPCAEstimator):
             self.logger.info(f"b={self.b}, c={self.c}")
             return self.bhat, self.chat
 
+    def _update_quadratic_parameters(self, sigma_nu):
+        #update internal parameters
+        self.sigma = np.sqrt(self.sigma**2 * sigma_nu**2)
+        self.bhat = self.compute_bhat(self.q, self.sigma)
+        self.chat = self.compute_chat(self.q, self.sigma)
+        #update sinkhorn 
+        self.sinkhorn._update_quadratic_parameters(sigma_nu,self.bhat,self.chat)
+        #update SVD
+        if attr_exists_not_none(self.svd,"S_"):
+            self.svd.S_ /= sigma_nu
+        if attr_exists_not_none(self.svd,"X_"):
+            self.svd.X_ /= sigma_nu
+        #update shrinker
+        self.shrinker.sigma = 1.
+
+        
     def compute_bhat(self, q, sigma):
         return multiply(subtract(1, q), square(sigma))
 

python/bipca/math.py
@@ -678,7 +678,25 @@ class Sinkhorn(BiPCAEstimator):
             self.column_error = ce
             self.fit_ = True
         return self
-
+    def _update_quadratic_parameters(self,sigma_nu,bhat,chat):
+        #update the object to reflect changes in sigma
+        # this occurs when sigma is updated in BiPCA by the shrinker.
+        # a modification by sigma_nu is multiplying the variance matrix by sigma_nu
+        # update the right scaling factor. Due to the update order, this is 
+        # the only change that propagates from an update to the variance matrix
+        self.right *= 1/sigma_nu
+        if attr_exists_not_none(self, "_Z"):
+            self.Z *= 1/sigma_nu
+        if attr_exists_not_none(self, "_var"):
+            self._var *= sigma_nu**2
+        #update the variance parameters 
+        if self.variance_estimator == "quadratic_2param":
+            self.bhat = bhat
+            self.chat = chat
+            self.c = self.compute_c(self.chat)
+            self.b = self.compute_b(self.bhat, self.c)
+            self.bhat = (self.b * self.P) / (1 + self.c)
+            self.chat = (1 + self.c - self.P) / (1 + self.c)
     def __set_operands(self, X=None):
         """DEPRECATED"""
         # changing the operators to accomodate for sparsity
@@ -1279,7 +1297,7 @@ class SVD(BiPCAEstimator):
 
         Parameters
         ----------
-        X : None, optional
+        X : None, optional_
             Description
 
         Returns
@@ -1959,6 +1977,7 @@ class SVD(BiPCAEstimator):
             V = V[None, :]
         return multiply(U, S) @ V.T
 
+  
 
 class Shrinker(BiPCAEstimator):
     """
@@ -2382,7 +2401,7 @@ class Shrinker(BiPCAEstimator):
             if "SVD" in self.A.uns.keys():
                 y = self.A.uns["SVD"]["S"]
             else:
-                y = self.A.uns["BiPCA"]["S_Z"]
+                y = self.A.uns["bipca"]["S"]
         try:
             check_is_fitted(self)
             try:
@@ -3705,7 +3724,28 @@ class MeanCenteredMatrix(BiPCAEstimator):
         # Convenience synonym for invert
         return self.invert(X)
 
+def library_normalize(X, scale='median'):
+    """library_normalize:
+    Normalize the data so that the rows sum to 1.
 
+    Parameters
+    ----------
+    X : array-like or AnnData
+        The input data to process.
+    scale : {numbers.Number, 'median'}, default 1
+        The scale factor to apply to the data
+    Returns
+    -------
+    Y : array-like or AnnData"""
+
+    libsize = np.asarray(sum(X, dim=1)).squeeze()
+    if scale == "median":
+        scale = np.median(libsize)
+    
+    
+    scale = scale / libsize[:,None]
+
+    return multiply(X, scale)
 def minimize_chebfun(p, domain=[0, 1]):
     start, stop = domain
     pd = p.differentiate()